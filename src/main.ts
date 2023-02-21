06389b7c3fabeeb42253df2accfa5b19eb06e89e
dileepkoppula <54669831+dileepkoppula@users.noreply.github.com>
2023-02-21 13:29:29 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-// import 'zone.js';
+import 'zone.js';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
